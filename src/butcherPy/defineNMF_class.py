8e68f2d3f3c722a5ac9ba631dd9fdcdfd8731dc6
aluisascosta <analuisascosta95@gmail.com>
2023-12-13 17:24:26 +0100

    added clust metrics and W normalisation

--- src/butcherPy/defineNMF_class.py
+++ src/butcherPy/defineNMF_class.py
@@ -8,6 +8,8 @@ Created by Ana Luisa Costa
 
 import matplotlib.pyplot as plt
 import numpy as np
+import sklearn as sk
+
 
 ## DEFINE THE NMF OBJECT CLASS -------------------------------------------------#
 class NMFobject:
@@ -35,18 +37,92 @@ class NMFobject:
         self.final_iterations = final_iterations
         self.frobenius = frobenius
     
-    # @ property
-    # def calc_rank_selection_metrics(self, 
-    #                                 metric = 'all'):
-    #     """
-    #     Calculates the rank selection metrics for each k. 
-    #     Metrics include:
-    #         - Frobenius norm (lower is better)
-    #         - Amari Distance (lower is better)
-    #         - Silhouette Width (higher is better)
-    #         - Cophenetic Correlation Coefficient (higher is better)
-    #     """
+    @ property
+    def define_signatures(self, 
+                          k = 'all', 
+                          exposure_threshold = 0.8):
+        """
+        Defines the k signatures from W matrix exposures
+        Input:
+            - k: selected rank (defaults as 'all')
+            - exposure_threshold: threshold for exposure values (from 0 to 1)
+            
+        Exposure threshold: from which normalised exposure value should
+        the observation be considered part of a signature at that rank.
+        (default is 0.8, lower values may result in observations being assigned 
+        to multiple signatures)
+        """
         
+    
+    @ property
+    def normalise_W(self):
+        """
+        Normalises the W matrix exposures for all ranks (sum of each column = 1)
+        """
+        # Initialise the normalised W matrix
+        W_norm = []
+        
+        # Verifies if the W matrix was already calculated for all ranks 
+        W_norm_sumtest = []
+        for i in range(len(self.W)):
+            W_norm_sumtest.append(np.sum(self.W[i], axis = 0))
+            
+        # If the W matrix was already normalised, return the W matrix
+        if np.all(np.isclose(W_norm_sumtest, 1)) == True:
+            print('W matrix was already normalised')
+            return self.W
+        
+        # Normalise the W matrix exposures by row
+        for i in range(len(self.W)):
+            W_norm.append(self.W[i] / np.sum(self.W[i], axis = 0))
+        
+        # Update the W matrix
+        self.W = W_norm    
+        
+    
+    
+    @ property
+    def calc_rank_selection_metrics(self, 
+                                    metric = 'all'):
+        
+        """
+        Calculates the rank selection metrics for each k. 
+        Metrics include:
+            - Frobenius norm (lower is better)
+            - Amari Distance (lower is better)
+            - Silhouette Width (higher is better)
+            - Cophenetic Correlation Coefficient (higher is better)
+        """
+        # Initialise the metrics 
+        frobenius = []
+        amari = []
+        silhouette = []
+        cophenetic = [] 
+            
+        # Calculate the metrics for each k
+        for i in range(len(self.W_eval)):
+            ### Calculate the frobenius norm
+            frobenius.append(np.linalg.norm(self.W_eval[i] - self.H[i]))
+            
+            ### Calculate the amari distance
+            dotp = np.abs(np.dot(self.W_eval[i], np.linalg.inv(self.H[i])))
+            # Compute the sum over each row and each column
+            sum_rows = np.sum(dotp, axis=0)
+            sum_cols = np.sum(dotp, axis=1)
+            # Compute the maximum value over each row and each column
+            max_rows = np.max(dotp / sum_rows, axis=0)
+            max_cols = np.max(dotp / sum_cols, axis=1)
+            # Compute the distance            
+            amari_distance =  (np.sum(max_rows) + np.sum(max_cols)) / (2.0 * dotp.shape[0])
+            amari.append(amari_distance)
+            
+            # Calculate the silhouette width
+            #silhouette.append(silhouette_width(self.W_eval[i], self.H[i]))
+            
+            # Calculate the cophenetic correlation coefficient
+            #cophenetic.append(cophenetic_correlation(self.W_eval[i], self.H[i]))
+
+    
         
     
     # @ property
