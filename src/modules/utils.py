4e1f757ffd4c60a137290cd19ce03e20d35a6793
LeonieBoland <leonie.b2000@gmail.com>
2024-07-04 12:07:00 +0200

    transform RData to AnnData

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -1,6 +1,22 @@
 import rds2py
+import anndata as ad
+import numpy as np
+from scipy.sparse import csr_matrix
+import warnings
 
 def setup_rds(path):
+    """
+    Trys to convert a type of matrix or experiment stored in a RData file to a numpy array.
+    
+    Parameters
+    ----------
+    path
+        string, a path to a RData file containing a matrix or experiment
+    
+    Returns
+    -------
+        numpy matrix extracted from the RData file
+    """
     r_obj = rds2py.read_rds(path)
     mat = None
 
@@ -27,5 +43,101 @@ def setup_rds(path):
 
     return mat
 
-def rds_to_ann():
-    print("RData object into anndata object")
+def rds_to_ann(path_to_rdsmatrix, path_to_rdsannot = None, path_to_rdsmeta = None, extra_metas = False, gene_annot = None, sample_annot = None, gene_columns = None):
+    """
+    Transforms RData to AnnData.
+
+    Parameters
+    ----------
+    path_to_rdsmatrix
+        string, the path to a RData file containing a gene expression matrix
+    path_to_rdsannot
+        string, the path to a RData file containing the gene annotations, the gene_annot gets priority if both are provided
+    path_to_rdsmeta
+        string, the path to a RData file containing the sample annotations, the sample_annot gets priority if both are provided
+    extra_metas
+        boolean, defining if further meta data stored in the path_to_rdsmeta file should be stored in the AnnData object
+    gene_annot
+        list of gene annotations 
+    sample_annot
+        list of sample annotations
+    gene_columns
+        a boolean stating if the gene expression matrix stores the genes in the columns (True) or if it stores the genes in the rows (False)
+    
+    Returns
+    -------
+    adata
+        AnnData object containing the provided information
+    """
+
+    #----------------------------------------------------------------------------------------------------------------#
+    #                            Get the gene expression matrix for AnnData, saved in X                              #
+    #----------------------------------------------------------------------------------------------------------------#
+    
+    # Get the gene expression matrix from the RData file
+    npmat = setup_rds(path_to_rdsmatrix)
+
+    # In AnnData the genes are stored in the columns, if that is not the case for the data at hand, the matrix is transposed
+    if gene_columns == False:
+        npmat = np.transpose(npmat)
+
+    # Check if the matrix has same column and row dimension
+    if npmat.shape[0] == npmat.shape[1] and gene_columns == None:
+        raise warnings.warn("The column and row dimension are equal, please make sure that the gene_columns parameter indicates, if the genes are saved in the columns (True) or not (False).")
+    
+    counts = csr_matrix(npmat, dtype=np.float32)
+    adata = ad.AnnData(counts)
+
+
+    #----------------------------------------------------------------------------------------------------------------#
+    #                Get the variable names (gene annotations) for AnnData saved in the columns                      #
+    #----------------------------------------------------------------------------------------------------------------#
+
+    if gene_annot != None:
+        gene_annots = gene_annot
+    
+    elif path_to_rdsannot != None:
+        annot = rds2py.read_rds(path_to_rdsannot)
+        # Gene annotations
+        gene_annots = annot['data'][1]['data']
+
+    else:
+        warnings.warn("You have not provided an annotation for the genes, by default the columns are numerated.")
+        gene_annots = [f"Gene_{i:d}" for i in range(adata.n_vars)]
+
+    if type(gene_annots) == list and len(gene_annots) != adata.n_vars:
+        warnings.warn("The provided gene annotation does not have the same dimension as the number of columns in the gene expression matrix, by default the columns are numerated. Check your data, especially if the matrix must be transposed and indicate it by the gene_columns parameter.")
+        gene_annots = [f"Gene_{i:d}" for i in range(adata.n_vars)]
+    
+    adata.var_names = gene_annots
+
+
+    #----------------------------------------------------------------------------------------------------------------#
+    #               Get the observation names (sample annotations) for AnnData saved in the rows                     #
+    #----------------------------------------------------------------------------------------------------------------#
+
+    if sample_annot != None:
+        sample_annots = sample_annot
+
+    elif path_to_rdsmeta != None:
+        meta = rds2py.read_rds(path_to_rdsmeta)
+        # Sample annotations
+        sample_annots = meta['data'][0]['data']
+
+    else:
+        warnings.warn("You have not provided an annotation for the samples, by default the rows are numerated.")
+        sample_annots = [f"Sample_{i:d}" for i in range(adata.n_obs)]
+
+    if type(sample_annots) == list and len(sample_annots) != adata.n_obs:
+        warnings.warn("The provided sample annotation has not the same dimension as the number of rows in the gene expression matrix, by default the rows are numerated. Check your data, especially if the matrix must be transposed and indicate it by the gene_columns parameter.")
+        sample_annots = [f"Sample_{i:d}" for i in range(adata.n_obs)]
+    else:
+        # Only if the sample annotations given in the RData file match the row dimension of the matrix the meta data can be used successfully
+        if extra_metas and path_to_rdsmeta != None and sample_annot == None:
+            metas = meta['attributes']['names']['data'][1:]
+            for i, m in enumerate(metas):
+                adata.obs[m] = meta['data'][i+1]['data'] 
+
+    adata.obs_names = sample_annots
+
+    return adata
